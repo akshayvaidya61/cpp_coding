7e59a54b5c6b1465c753d1f1fedb7ad1e07f5851
Akshay Vaidya <akshay.s.vaidya@gmail.com>
2024-08-05 12:11:44 -0400

    * Created 2 output NV12 and UYVY. * Added node to graph.

--- cross_compile_to_tda4/apps/viss_with_imx390/main.cpp
+++ cross_compile_to_tda4/apps/viss_with_imx390/main.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include <array>
 #include <string>
 #include <iostream>
 #include <thread>
@@ -21,15 +21,15 @@ extern "C"
 #include "TI/tivx_log_rt.h"
 }
 
-static constexpr uint32_t AE_GAIN_MAGIC_VALUE = 1030;
-static constexpr uint32_t AE_EXPOSURE_TIME_MAGIC_VALUE = 16666;
-static constexpr uint32_t AWB_TEMPERATURE_MAGIC_VALUE = 3000;
-static constexpr uint32_t AWB_GAIN_MAGIC_VALUE = 525;
-static constexpr uint32_t AWB_OFFSET_MAGIC_VALUE = 2;
-static constexpr uint32_t META_HEIGHT_AFTER = 4;
-static constexpr uint32_t NUM_EXPOSURES = 1;
-static constexpr uint32_t MSB_16_BIT = 11;
-static constexpr uint32_t META_HEIGHT_BEFORE = 0;
+static constexpr uint32_t AE_GAIN_MAGIC_VALUE = 1030U;
+static constexpr uint32_t AE_EXPOSURE_TIME_MAGIC_VALUE = 16666U;
+static constexpr uint32_t AWB_TEMPERATURE_MAGIC_VALUE = 3000U;
+static constexpr uint32_t AWB_GAIN_MAGIC_VALUE = 525U;
+static constexpr uint32_t AWB_OFFSET_MAGIC_VALUE = 2U;
+static constexpr uint32_t META_HEIGHT_AFTER = 4U;
+static constexpr uint32_t NUM_EXPOSURES = 1U;
+static constexpr uint32_t MSB_16_BIT = 11U;
+static constexpr uint32_t META_HEIGHT_BEFORE = 0U;
 static constexpr const char *H3A_DATA_OBJECT_NAME = "tivx_h3a_data_t";
 static constexpr size_t H3A_DATA_OBJECT_SIZE = sizeof(tivx_h3a_data_t);
 static constexpr const char *CONFIGURATION_OBJECT_NAME = "tivx_vpac_viss_params_t";
@@ -40,14 +40,23 @@ static constexpr const char *DCC_PARAM_VISS_OBJECT_NAME = "dcc_viss";
 static constexpr auto VISS_TARGET = TIVX_TARGET_VPAC_VISS1;
 static constexpr auto VISS_H3A_IN_LSC = TIVX_VPAC_VISS_H3A_IN_LSC;
 static constexpr auto VISS_H3A_MODE_AEWB = TIVX_VPAC_VISS_H3A_MODE_AEWB;
-static constexpr auto VISS_SENSOR_DCC_ID_390 = 390;
+static constexpr auto VISS_SENSOR_DCC_ID_390 = 390U;
 static constexpr auto VISS_EE_MODE_OFF = TIVX_VPAC_VISS_EE_MODE_OFF;
 static constexpr auto VISS_MUX2_NV12 = TIVX_VPAC_VISS_MUX2_NV12;
 static constexpr auto VISS_CHROMA_MODE_420 = TIVX_VPAC_VISS_CHROMA_MODE_420;
 static constexpr auto VISS_MUX2_YUV422 = TIVX_VPAC_VISS_MUX2_YUV422;
 static constexpr auto VISS_CHROMA_MODE_422 = TIVX_VPAC_VISS_CHROMA_MODE_422;
-static constexpr auto RAW_IMG_WIDTH = 1936;
-static constexpr auto RAW_IMG_HEIGHT = 1096;
+static constexpr auto RAW_IMG_WIDTH = 1936U;
+static constexpr auto RAW_IMG_HEIGHT = 1096U;
+static constexpr auto IMG_HEIGHT = 1096U;
+static constexpr auto IMG_WIDTH = 1936U;
+static constexpr auto VISS_OUT_NUM = 2U;
+
+vx_node const vxCreateVissWithImx390Node(vx_graph, vx_context, tivx_raw_image, std::array<vx_image, VISS_OUT_NUM>);
+vx_user_data_object createDccParamViss(vx_context context, const char *sensor_name, uint32_t sensor_dcc_mode);
+vx_user_data_object createAeAwbResult(vx_context context);
+vx_user_data_object createH3aAewAf(vx_context context);
+tivx_raw_image createRawImage(vx_context context, vx_uint32 width, vx_uint32 height);
 
 bool isExitRequested = false;
 void signal_handler(int signal)
@@ -55,16 +64,10 @@ void signal_handler(int signal)
     isExitRequested = true;
 }
 
-vx_node const vxCreateVissWithImx390Node(vx_graph, vx_context);
-vx_user_data_object createDccParamViss(vx_context context, const char *sensor_name, uint32_t sensor_dcc_mode);
-vx_user_data_object createAeAwbResult(vx_context context);
-vx_user_data_object createH3aAewAf(vx_context context);
-
 int main()
 {
 
     std::signal(SIGINT, signal_handler);
-
     std::string filename;
 
     appInit();
@@ -76,24 +79,45 @@ int main()
         std::cout << "Failed to create context" << std::endl;
     }
 
+    tivxHwaLoadKernels(context);
+
     vx_graph graph;
     if (graph = vxCreateGraph(context); vxGetStatus(reinterpret_cast<vx_reference>(graph)) != VX_SUCCESS)
     {
         std::cout << "Failed to create graph" << std::endl;
     }
 
+    tivx_raw_image input = createRawImage(context, RAW_IMG_WIDTH, RAW_IMG_HEIGHT);
+
+    std::array<vx_image, VISS_OUT_NUM> listOfOutputImages;
+    listOfOutputImages.at(0) = vxCreateImage(context, IMG_WIDTH, IMG_HEIGHT, VX_DF_IMAGE_NV12);
+    listOfOutputImages.at(1) = vxCreateImage(context, IMG_WIDTH, IMG_HEIGHT, VX_DF_IMAGE_UYVY);
+
     vx_node viss_node;
-    if (viss_node = vxCreateVissWithImx390Node(graph, context); vxGetStatus(reinterpret_cast<vx_reference>(viss_node)) != VX_SUCCESS)
+    if (viss_node = vxCreateVissWithImx390Node(graph, context, input, listOfOutputImages); vxGetStatus(reinterpret_cast<vx_reference>(viss_node)) != VX_SUCCESS)
     {
         std::cout << "Failed to create viss node" << std::endl;
     }
 
-    vx_image input;
-    if (input = vxCreateImage(context, 1280, 960, VX_DF_IMAGE_U8); vxGetStatus(reinterpret_cast<vx_reference>(input)) != VX_SUCCESS)
+    if (auto result = vxSetNodeTarget(viss_node, VX_TARGET_STRING, TIVX_TARGET_VPAC_VISS1); result != VX_SUCCESS)
     {
-        std::cout << "Failed to create input image" << std::endl;
-        std::runtime_error("Failed to create input image");
+        std::cout << "Failed to set node targets" << std::endl;
     }
+
+    if (auto result = vxVerifyGraph(graph); result != VX_SUCCESS)
+    {
+        std::cout << "Failed to verify graph" << std::endl;
+    }
+
+    uint64_t frameCount = 0U;
+    while (!isExitRequested)
+    {
+        vxProcessGraph(graph);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "running. frame-index: " << frameCount++ << std::endl;
+    }
+
+    tivxHwaUnLoadKernels(context);
 
     tivxDeInit();
     appDeInit();
@@ -183,45 +207,39 @@ vx_user_data_object createH3aAewAf(vx_context context)
     return h3a_aew_af;
 }
 
-vx_node createVissNode(vx_graph graph, vx_context context, const tivx_vpac_viss_params_t *params, tivx_raw_image raw, vx_image y8_r8_c2)
+vx_node createVissNode(vx_graph graph, vx_context context, const tivx_vpac_viss_params_t *params, tivx_raw_image input, std::array<vx_image, VISS_OUT_NUM> listOfOutputImages)
 {
     vx_user_data_object configuration = vxCreateUserDataObject(context, CONFIGURATION_OBJECT_NAME, CONFIGURATION_OBJECT_SIZE, params);
     vx_user_data_object ae_awb_result = createAeAwbResult(context);
     vx_user_data_object dcc_param_viss = createDccParamViss(context, SENSOR_SONY_IMX390_UB953_D3, 0);
     vx_user_data_object h3a_aew_af = createH3aAewAf(context);
 
-    return tivxVpacVissNode(graph, configuration, ae_awb_result, dcc_param_viss, raw, NULL, NULL, y8_r8_c2, NULL, NULL, h3a_aew_af, NULL, NULL, NULL);
+    return tivxVpacVissNode(graph, configuration, ae_awb_result, dcc_param_viss, input, listOfOutputImages.at(0), NULL, NULL, NULL, NULL, h3a_aew_af, NULL, NULL, NULL);
 }
 
-vx_node const vxCreateVissWithImx390Node(vx_graph currentGraph, vx_context currentContext)
+vx_node const vxCreateVissWithImx390Node(vx_graph currentGraph, vx_context currentContext, tivx_raw_image input, std::array<vx_image, VISS_OUT_NUM> output)
 {
     vx_node node = nullptr;
     if (vx_true_e == tivxIsTargetEnabled(VISS_TARGET))
     {
-        tivxHwaLoadKernels(currentContext);
-
-        tivx_raw_image raw = createRawImage(currentContext, RAW_IMG_WIDTH, RAW_IMG_HEIGHT);
-        vx_uint32 width, height;
-        tivxQueryRawImage(raw, TIVX_RAW_IMAGE_WIDTH, &width, sizeof(width));
-        tivxQueryRawImage(raw, TIVX_RAW_IMAGE_HEIGHT, &height, sizeof(height));
-
-        vx_image y8_r8_c2 = vxCreateImage(currentContext, width, height, VX_DF_IMAGE_NV12);
-
         tivx_vpac_viss_params_t params;
         tivx_vpac_viss_params_init(&params);
         params.h3a_in = VISS_H3A_IN_LSC;
         params.h3a_aewb_af_mode = VISS_H3A_MODE_AEWB;
         params.sensor_dcc_id = VISS_SENSOR_DCC_ID_390;
-        params.bypass_glbce = 0;
-        params.bypass_nsf4 = 0;
+        params.bypass_glbce = 1U;
+        params.bypass_nsf4 = 1U;
         params.fcp[0].ee_mode = VISS_EE_MODE_OFF;
-        params.fcp[0].mux_output2 = VISS_MUX2_NV12;
         params.fcp[0].chroma_mode = VISS_CHROMA_MODE_420;
         params.fcp[1].ee_mode = VISS_EE_MODE_OFF;
-        params.fcp[1].mux_output2 = VISS_MUX2_YUV422;
         params.fcp[1].chroma_mode = VISS_CHROMA_MODE_422;
+        params.fcp[0].mux_output2 = VISS_MUX2_NV12;
+        params.fcp[1].mux_output2 = VISS_MUX2_YUV422;
 
-        node = createVissNode(currentGraph, currentContext, &params, raw, y8_r8_c2);
+        params.output_fcp_mapping[0] = TIVX_VPAC_VISS_MAP_FCP_OUTPUT(TIVX_VPAC_VISS_FCP0, TIVX_VPAC_VISS_FCP_OUT2);
+        params.output_fcp_mapping[2] = TIVX_VPAC_VISS_MAP_FCP_OUTPUT(TIVX_VPAC_VISS_FCP1, TIVX_VPAC_VISS_FCP_OUT2);
+
+        node = createVissNode(currentGraph, currentContext, &params, input, output);
 
         vxSetNodeTarget(node, VX_TARGET_STRING, VISS_TARGET);
     }
